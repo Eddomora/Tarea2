d9d34ab664481a420f16b68797d83bbb05ed9834
Vicente <vimiranda2023@udec.cl>
2025-05-12 13:26:56 -0400

    Agregación y modificación de clases principales

---
+++ Proyecto2/src/main/java/org/example/Empleado.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class Empleado {
+
+    private String id;
+    private String apellidos;
+    private String nombre;
+    private String correo;
+    private Departamento departamento;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+}
---
+++ Proyecto2/src/main/java/org/example/Nota.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import java.time.LocalDateTime;
+
+public class Nota {
+    private String contenido;
+    private LocalDateTime horaFecha;
+}
