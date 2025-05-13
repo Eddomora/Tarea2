3ce49a36b509818f80ba1f7761770900cf841d6b
Liivne <ignsoto2023@inf.udec.cl>
2025-05-13 03:16:02 -0400

    Clase reunión con métodos vacíos
    ReunionPresencial y ReunionVirtual extienden de Reunion

--- Proyecto2/src/main/java/org/example/ReunionPresencial.java
+++ Proyecto2/src/main/java/org/example/ReunionPresencial.java
@@ -1,5 +1,15 @@
 package org.example;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
-public class ReunionPresencial {
+public class ReunionPresencial extends Reunion {
     private String sala;
+
+    public ReunionPresencial(Date fecha, Instant horaPre, Duration duracionPre, Instant horaIni, Instant horaFin, String sala) {
+        super(fecha, horaPre, duracionPre, horaIni, horaFin, sala);
+        this.sala = sala;
+    }
 }
